e799456f1ab1f6a340c948a3502efa571f01c756
OumaymaBrd <oumaymalovemaths>
2025-02-21 15:35:03 +0100

    Regler le modal User

--- app/Models/User.php
+++ app/Models/User.php
@@ -24,4 +24,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
+
+    public function isWriter()
+    {
+        return $this->role === 'writer';
+    }
 }
