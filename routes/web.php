1ca0deb0e08227d8151c2c1ef121d773484d0200
OumaymaBrd <oumaymalovemaths>
2025-02-21 15:47:40 +0100

    modifier route

--- routes/web.php
+++ routes/web.php
@@ -17,16 +17,18 @@ Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LogoutController::class, 'logout'])->name('logout');
 
 Route::middleware(['auth'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-
     Route::get('/articles', [ArticleController::class, 'index'])->name('articles.index');
-    Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
-    Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
     Route::get('/articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
     Route::post('/articles/{article}/like', [ArticleController::class, 'like'])->name('articles.like');
     Route::post('/articles/{article}/dislike', [ArticleController::class, 'dislike'])->name('articles.dislike');
 });
 
+Route::middleware(['auth', 'writer'])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 
+    Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
+    Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
+    Route::get('/my-articles', [ArticleController::class, 'myArticles'])->name('articles.my_articles');
+});
